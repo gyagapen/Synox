9c2b21e6a63b018c4b49a39da1d731c9bdd98db6
Emmanuel YAGAPEN <Gui@Gui-PC.(none)>
2012-01-10 13:53:16 +0100

    modification du script de la BD, creation du dossier ScriptBD

--- UserInterface/InterfaceGraphiqueSMS/InterfaceGraphiqueSMS/Default.aspx.cs
+++ UserInterface/InterfaceGraphiqueSMS/InterfaceGraphiqueSMS/Default.aspx.cs
@@ -9,8 +9,25 @@ namespace InterfaceGraphiqueSMS
 {
     public partial class _Default : System.Web.UI.Page
     {
+
+        //contexte de la base de donnees
+        SMSBDDataContext dbContext = new SMSBDDataContext();
+        
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
+
+        protected void EcrireSMS(object sender, EventArgs e)
+        {
+            //insertion d'un message
+            Message msg = new Message();
+            msg.messagePDU = contenuSMS.Text;
+            msg.noDestinataire = numDestinataire.Text;
+
+            dbContext.Message.InsertOnSubmit(msg);
+            dbContext.SubmitChanges();
 
         }
     }
---
+++ UserInterface/InterfaceGraphiqueSMS/InterfaceGraphiqueSMS/EcrireSMS.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="InterfaceGraphiqueSMS._Default" MasterPageFile="~/Base.Master" %>
+
+
+<asp:Content ContentPlaceHolderID=Main  runat=server>
+
+    <!-- Formulaire d envoi -->
+   
+
+    <form method=get>
+        Num&eacute;ro de t&eacute;l&eacute;phone :
+        <asp:TextBox runat=server ID="numDestinataire"></asp:TextBox><br />
+        Message à envoyer : 
+        <hr />
+        <asp:TextBox TextMode=MultiLine ID="contenuSMS" Height="128px" Width="455px" runat="server"></asp:TextBox><br />
+        <asp:Button Text="Valider" onclick="EcrireSMS" runat="server"/>
+    </form>
+
+</asp:Content>
