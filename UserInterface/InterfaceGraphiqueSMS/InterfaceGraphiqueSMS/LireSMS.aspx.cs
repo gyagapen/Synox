eb6554bae423e30f1d04f4aeed72364a65ee5378
Guillaume Leroy <Guillaume@Guillaume-PC.(none)>
2012-01-11 15:13:26 +0100

    Debut de l'interface pour la lecture des SMS

---
+++ UserInterface/InterfaceGraphiqueSMS/InterfaceGraphiqueSMS/LireSMS.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InterfaceGraphiqueSMS
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SMSBDDataContext dbContext = new SMSBDDataContext();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Message[] listeMessages;
+            listeMessages = (from mess in dbContext.Message select mess).ToArray();
+            ListMessages.DataSource = listeMessages;
+            ListMessages.DataTextField = "messageTexte";
+            ListMessages.DataValueField = "idMessage";
+            ListMessages.DataBind();
+            tbMessage.Text = "troc";
+        }
+
+        protected void ListMessages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tbMessage.Text = "truc";
+        }
+    }
+}
